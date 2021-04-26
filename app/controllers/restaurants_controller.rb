cf2b4ee60147f17bd6f9e1ffbe9e350583faaa55
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-04-26 12:03:44 +0100

    review controller created && nested in routes && View, and model set up

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,39 +1,44 @@
 class RestaurantsController < ApplicationController
 
-  before_action :set_restaurant, only: [ :show]
+  #before_action :set_restaurant, only: [ :show]
 
-
-  def index
+   def index
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
- def new
+  def new
     @restaurant = Restaurant.new
   end
 
-  def show
-  end
-
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
-
     if @restaurant.save
-      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+      redirect_to restaurant_path(@restaurant.id)
     else
       render :new
     end
   end
 
+  # def create
+    # @restaurant = Restaurant.new(restaurant_params)
+    # if @restaurant.save
+     # redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    # else
+     # render :new
+    # end
+  # end
+
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-
+    #def set_restaurant
+      #@restaurant = Restaurant.find(params[:id])
+    #end
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-    end
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
